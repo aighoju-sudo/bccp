5641114d55892c3c2a8881c7db248c06f4e7a126
aighoju-sudo <aighoju-sudo@users.noreply.github.com>
2026-08-31 00:49:04 +0900

    ㅁㅁ

--- server.js
+++ server.js
@@ -6,17 +6,21 @@ const publicDir = path.join(__dirname, 'public');
 
 app.use(express.static(publicDir));
 
+//===============================
 app.get('/', function (req, res) {
   res.sendFile(path.join(publicDir, 'index.html'));
-});
-
-app.get('/home.html', function (req, res) {
-  res.sendFile(path.join(publicDir, 'home.html'));
 });
 
 app.get('/index.html', function (req, res) {
   res.sendFile(path.join(publicDir, 'index.html'));
 });
+//===============================
+
+// app.get('/home.html', function (req, res) {
+//   res.sendFile(path.join(publicDir, 'home.html'));
+// });
+
+
 
 app.listen(8080, function () {
   console.log('listening on 8080');
